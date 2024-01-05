6601c924965d1469421737487554d84e1c70405e
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-01-05 18:59:23 +0530

    added cards for todo pomodoro notes and palette in home page

--- utility-app/client/src/pages/HomePage.jsx
+++ utility-app/client/src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
 const HomePage = () => {
   return (
-    <div className="px-6">
+    <div className="p-6 w-[96%] mx-auto bg-base-300 rounded-xl">
       <h1 className="text-2xl">Home</h1>
+
+      <div className="grid grid-cols-4 gap-4 mt-4">
+        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+          Todos
+        </div>
+        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+          Notes
+        </div>
+        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+          Pomodoro
+        </div>
+        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+          Color Palette
+        </div>
+      </div>
     </div>
   );
 };

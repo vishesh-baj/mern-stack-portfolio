056e26bbdb2ebd68539404f17a8d736d3e91e1f5
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-01-06 17:08:33 +0530

    added changes

--- utility-app/client/src/pages/HomePage.jsx
+++ utility-app/client/src/pages/HomePage.jsx
@@ -4,16 +4,16 @@ const HomePage = () => {
       <h1 className="text-2xl">Home</h1>
 
       <div className="grid grid-cols-4 gap-4 mt-4">
-        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+        <div className="col-span-4 md:col-span-1 p-4 bg-primary rounded-xl text-black font-semibold">
           Todos
         </div>
-        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+        <div className="col-span-4 md:col-span-1 p-4 bg-secondary rounded-xl text-black font-semibold">
           Notes
         </div>
-        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+        <div className="col-span-4 md:col-span-1 p-4 bg-accent rounded-xl text-black font-semibold">
           Pomodoro
         </div>
-        <div className="col-span-4 md:col-span-1 p-4 bg-base-100 rounded-xl">
+        <div className="col-span-4 md:col-span-1 p-4 bg-warning rounded-xl text-black font-semibold">
           Color Palette
         </div>
       </div>

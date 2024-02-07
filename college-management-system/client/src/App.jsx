28f504c7e0c7b5cb106b39eb8dbcfc2eb2eec95c
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-02-07 19:33:44 +0530

    added changes

--- college-management-system/client/src/App.jsx
+++ college-management-system/client/src/App.jsx
@@ -1,3 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { DashboardPage, HomePage } from "./pages";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </h1>
+  );
 }

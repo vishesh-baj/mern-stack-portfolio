5f0614ded3d9cdb3ae5b76bef7e146d2d7418e03
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-03-07 18:02:25 +0530

    added changes to quotes page

--- utility-app/client/src/components/QuoteCard.jsx
+++ utility-app/client/src/components/QuoteCard.jsx
@@ -1,38 +1,39 @@
 import PropTypes from "prop-types";
 import { FaHeart } from "react-icons/fa";
-import { useMutation, useQuery } from "react-query";
-import { API_INSTANCE } from "../api";
-const QuoteCard = ({ quoteData }) => {
-  const fetchQuotes = async () => {
-    const response = await API_INSTANCE.get("quote/get-all-quotes");
-    return response.data;
-  };
-  const addQuoteMutation = useMutation(
-    (data) => API_INSTANCE.post("quote/add-quote", data),
-    {
-      onSuccess: (data) => console.log("DATA: ", data),
-    }
-  );
-
-  const { data: fetchedQuotes, isLoading } = useQuery(
-    "fetch-quotes",
-    fetchQuotes,
-    { onSuccess: (data) => console.log("QUOTE DATA: :", data) }
-  );
-
+import { MdDeleteOutline } from "react-icons/md";
+const QuoteCard = ({
+  quoteData,
+  handleMutation,
+  handleDeleteMutation,
+  showSave,
+}) => {
   const handleSaveQuote = () => {
     console.log(quoteData);
-    addQuoteMutation.mutate(quoteData);
+    handleMutation.mutate(quoteData);
   };
 
+  const handleDeleteQuote = (id) => {
+    console.log("DELETE QUOTE ");
+    handleDeleteMutation.mutate(id);
+  };
+  
   return (
     <div className="bg-base-200 px-4 py-2 rounded-xl">
       <h2 className="text-lg">{quoteData?.content}</h2>
       <div className="flex justify-between items-center">
         <p className="font-semibold">- {quoteData.author}</p>
-        <button onClick={handleSaveQuote} className="btn btn-circle">
-          <FaHeart />
-        </button>
+        {showSave ? (
+          <button onClick={handleSaveQuote} className="btn btn-circle">
+            <FaHeart />
+          </button>
+        ) : (
+          <button
+            className="btn btn-circle "
+            onClick={() => handleDeleteQuote(quoteData._id)}
+          >
+            <MdDeleteOutline className="text-2xl text-red-400" />
+          </button>
+        )}
       </div>
     </div>
   );
@@ -40,9 +41,13 @@ const QuoteCard = ({ quoteData }) => {
 
 QuoteCard.propTypes = {
   quoteData: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }),
+  showSave: PropTypes.bool.isRequired,
+  handleMutation: PropTypes.func,
+  handleDeleteMutation: PropTypes.func,
 };
 
 export default QuoteCard;
--- utility-app/client/src/pages/QuotesPage.jsx
+++ utility-app/client/src/pages/QuotesPage.jsx
@@ -1,24 +1,67 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import SectionLayout from "../layout/SectionLayout";
 import { QUOTES_API_ENDPOINT } from "../constants";
 import axios from "axios";
 import QuoteCard from "../components/QuoteCard";
 import Loader from "../components/Loader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { API_INSTANCE } from "../api";
+import toast from "react-hot-toast";
+import { IoCloudDownloadOutline } from "react-icons/io5";
 const QuotesPage = () => {
   const [activeTab, setActiveTab] = useState("browse");
+  const [quotesList, setQuotesList] = useState([]);
+  const [loadMoreTrigger, setLoadMoreTrigger] = useState(false);
   const fetchQuotes = async () => {
     const response = await axios.get(QUOTES_API_ENDPOINT);
     return response.data;
   };
-
-  const { data: fetchedQuotesData, isLoading } = useQuery(
+  const fetchQuotesDB = async () => {
+    const response = await API_INSTANCE.get("quote/get-all-quotes");
+    return response.data;
+  };
+  const { isLoading, refetch: quotesRefetch } = useQuery(
     "quotes",
     fetchQuotes,
     {
-      onSuccess: (data) => console.log("DATA: ", data),
+      onSuccess: (data) =>
+        setQuotesList((prevState) => [...prevState, ...data]),
     }
   );
+
+  const { data: fetchedQuotesFromDB, refetch } = useQuery(
+    "fetch-quotes",
+    fetchQuotesDB
+  );
+
+  const addQuoteMutation = useMutation(
+    (data) => API_INSTANCE.post("quote/add-quote", data),
+    {
+      onSuccess: () => {
+        toast.success("Quote Added Successfully");
+        refetch();
+      },
+    }
+  );
+
+  const deleteQuoteMutation = useMutation(
+    (id) => API_INSTANCE.delete(`quote/delete-quote/${id}`),
+    {
+      onSuccess: () => {
+        toast.success("Quote Deleted Successfully");
+        refetch();
+      },
+    }
+  );
+
+  const handleLoadMore = () => {
+    console.log("LOAD MORE");
+    setLoadMoreTrigger((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    quotesRefetch();
+  }, [loadMoreTrigger, quotesRefetch]);
 
   return (
     <SectionLayout sectionTitle="Random Quotes">
@@ -42,9 +85,24 @@ const QuotesPage = () => {
             <Loader />
           ) : (
             <div className="flex flex-col gap-4 mt-4 overflow-y-auto max-h-96">
-              {fetchedQuotesData?.map((quote) => {
-                return <QuoteCard quoteData={quote} key={quote._id} />;
+              {quotesList?.map((quote) => {
+                return (
+                  <QuoteCard
+                    quoteData={quote}
+                    handleMutation={addQuoteMutation}
+                    showSave
+                    key={quote._id}
+                  />
+                );
               })}
+              <div
+                className="flex justify-center w-full tooltip tooltip-bottom"
+                data-tip="load more"
+              >
+                <button onClick={handleLoadMore} className="btn btn-square">
+                  <IoCloudDownloadOutline className="text-2xl text-teal-400" />
+                </button>
+              </div>
             </div>
           )}
         </div>
@@ -64,7 +122,17 @@ const QuotesPage = () => {
             activeTab === "saved" ? "" : "hidden"
           }`}
         >
-          Tab content 2
+          <div className="flex flex-col gap-4 mt-4 overflow-y-auto max-h-96">
+            {fetchedQuotesFromDB?.allQuotes.map((quote) => {
+              return (
+                <QuoteCard
+                  key={quote._id}
+                  quoteData={quote}
+                  handleDeleteMutation={deleteQuoteMutation}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </SectionLayout>

289d7326dd86980fce43d37e2c18bfeb2e0f6362
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-03-21 21:54:39 +0530

    added change t oquiz api

--- utility-app/server/controllers/quizController.js
+++ utility-app/server/controllers/quizController.js
@@ -2,6 +2,8 @@ import Quiz from "../schema/quiz.js";
 
 export const getAllQuiz = async (req, res) => {
   try {
+    const allQuiz = await Quiz.find({});
+    res.status(200).json(allQuiz);
   } catch (error) {
     res.status(400).json({ error: error.message, message: "error occured " });
   }

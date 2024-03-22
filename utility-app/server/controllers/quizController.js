c60d70296c8275e57cdfb82a42231b37bd52d30c
vishesh-baj <vishesh.bajpayee@icloud.com>
2024-03-22 17:57:54 +0530

    added changes

--- utility-app/server/controllers/quizController.js
+++ utility-app/server/controllers/quizController.js
@@ -1,11 +1,24 @@
 import Quiz from "../schema/quiz.js";
 
 export const getAllQuiz = async (req, res) => {
-  const user = req.user;
   try {
     const allQuiz = await Quiz.find({ user: req.user });
+    if (!allQuiz) {
+      return res.status(400).json({ message: "Unable to get all quizes" });
+    }
     res.status(200).json(allQuiz);
   } catch (error) {
     res.status(400).json({ error: error.message, message: "error occured " });
+  }
+};
+
+export const addQuiz = async (req, res) => {
+  try {
+    const newQuiz = new Quiz({ ...req.body, user: req.user });
+    res
+      .status(200)
+      .json({ message: "Quiz added successfully", addedQuiz: newQuiz });
+  } catch (error) {
+    res.status(400).json({ error: error.message, message: "error occured" });
   }
 };
